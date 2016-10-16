68acfa1b77d70b6f1885c571fc1d2087dca737cc
Nic Adams <nic.adams@hotmail.com>
2016-10-16 03:19:16 +0000

    added some style and class

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const methodOverride = require('method-override');
 const bcrypt = require('bcrypt');
 const cookieSession = require('cookie-session');
